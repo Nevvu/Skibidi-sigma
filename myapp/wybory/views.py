6b6c301d9c1e7c3971515eedbf54969cd068ce61
Mateusz <mateuszmilewski2408@gmail.com>
2025-05-15 20:39:06 +0200

    Nie przekazuje do bazy kto na kogo glosowal

--- myapp/wybory/views.py
+++ myapp/wybory/views.py
@@ -111,6 +111,37 @@ def cast_vote(request, election_id):
         form = CastVoteForm(election=election)
 
     return render(request, 'wybory/voter/cast_vote.html', {'election': election, 'form': form})
+
+# @login_required
+# def cast_vote(request, election_id):
+#     election = Election.objects.filter(id=election_id, date__gte=datetime.date.today()).first()
+#     if not election:
+#         messages.error(request, "Nie znaleziono wyborów lub są one niedostępne.")
+#         return redirect('voter_panel')
+
+
+#     if request.session.get(f'voted_{election_id}', False):
+#         messages.error(request, "Już oddałeś głos w tych wyborach.")
+#         return redirect('voter_panel')
+
+#     if request.method == 'POST':
+#         form = CastVoteForm(request.POST, election=election)
+#         if form.is_valid():
+#             candidate = form.cleaned_data['candidate']
+
+#             Vote.objects.create(candidate=candidate, election=election)
+
+
+#             request.session[f'voted_{election_id}'] = True
+
+#             messages.success(request, "Twój głos został oddany pomyślnie.")
+#             return redirect('voter_panel')
+#     else:
+#         form = CastVoteForm(election=election)
+
+#     return render(request, 'wybory/voter/cast_vote.html', {'election': election, 'form': form})
+
+
 # --- Voter-only views ---
 @login_required
 def voter_panel(request): return render(request, 'wybory/voter/panel.html')
