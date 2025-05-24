ec8e77d5bc1cf079df92586cb03779497ddf858a
Michał Sienicki <msienicki97@gmail.com>
2025-05-24 20:03:55 +0200

    Zmiany w plikach, czyszczenie, pusta baza

--- myapp/myapp/settings.py
+++ myapp/myapp/settings.py
@@ -106,12 +106,8 @@ WSGI_APPLICATION = "myapp.wsgi.application"
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'wybory', 
-        'USER': 'admin1', 
-        'PASSWORD': 'admin1',        
-        'HOST': 'localhost',        
-        'PORT': '5432',            
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
